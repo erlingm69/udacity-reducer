cf21c920b0daea552cbdfb117b139818c3324f76
Erling Mårtensson <erling.martensson@gmail.com>
2021-05-27 14:54:38 +0200

    refactor: Use cra file structure

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux'
+import todos from '../reducers/todos'
+import loading from '../reducers/loading'
+import goals from '../reducers/goals'
+
+export default combineReducers({
+    todos,
+    goals,
+    loading,
+  })
